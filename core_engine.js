99596dee021e1a95b289570ab0520b59ad28184e
bp6gl41123 <e1p3m06@gmail.com>
2026-03-05 23:28:05 +0800

    Add files via upload

--- core_engine.js
+++ core_engine.js
@@ -2,7 +2,7 @@
 /* ==== 【組件 E：核心引擎 - core_engine.js】 ==== */
 /* ============================================================== */
 
-const DB_KEY = 'DashboardDB_V79_Final'; 
+const DB_KEY = 'DashboardDB_V80_Final'; 
 window.dataDB = JSON.parse(localStorage.getItem(DB_KEY));
 window.isNegativeMode = false; // 🪄 魔法反向開關
 
